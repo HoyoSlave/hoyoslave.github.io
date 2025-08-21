bcb57b230a8e14adc8c860b89d9f1d619bba6097
AlfanaSofyan / HoyoSlave <azazil.exe@gmail.com>
2025-08-21 19:19:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Initialize all functionality
   initializeScrollAnimations()
-  initializeSmoothScroll()
 })
 
 // Scroll Animations (up and down)
 function initializeScrollAnimations() {
   const observerOptions = {
-    threshold: 0.1,
-    rootMargin: "0px 0px 50px 0px",
+    rootMargin: "25px 25px 25px 25px",
   }
 
   const observer = new IntersectionObserver((entries) => {
@@ -28,28 +26,5 @@ function initializeScrollAnimations() {
   const fadeElements = document.querySelectorAll(".fade-in")
   fadeElements.forEach((element) => {
     observer.observe(element)
-  })
-}
-
-// Smooth Scroll
-function initializeSmoothScroll() {
-  const links = document.querySelectorAll('a[href^="#"]')
-
-  links.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault()
-
-      const targetId = this.getAttribute("href")
-      const targetSection = document.querySelector(targetId)
-
-      if (targetSection) {
-        const offsetTop = targetSection.offsetTop - 70 // Account for fixed navbar
-
-        window.scrollTo({
-          top: offsetTop,
-          behavior: "smooth",
-        })
-      }
-    })
   })
 }
